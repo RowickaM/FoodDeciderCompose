abf039fb64cab410091fae71327b4cfeaa653750
rowickam <rowickam.23@gmail.com>
2021-07-19 09:58:30 +0200

    create dummy data

--- app/src/main/java/pl/gungnir/fooddecider/MainActivity.kt
+++ app/src/main/java/pl/gungnir/fooddecider/MainActivity.kt
@@ -4,35 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import pl.gungnir.fooddecider.mics.Toolbar
 import pl.gungnir.fooddecider.screens.randomizeFood.RandomizeFood
-import pl.gungnir.fooddecider.screens.savedFood.SavedFood
 import pl.gungnir.fooddecider.ui.theme.FoodDeciderTheme
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val list = arrayListOf<String>()
-        for (i in 0..15) {
-            list.add("Jedzenie pozycja $i")
-        }
         setContent {
 
             FoodDeciderTheme {
@@ -41,9 +23,7 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize()
                         .background(color = MaterialTheme.colors.background)
                 ) {
-                    SavedFood(
-                        list = list
-                    )
+                    RandomizeFood()
                 }
             }
         }
