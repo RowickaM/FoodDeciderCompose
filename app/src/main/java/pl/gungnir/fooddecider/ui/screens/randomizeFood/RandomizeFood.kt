d5a0048eff8f6f4b9cda3a3c7d298145d1621b5b
Magdalena Rowicka <mrowicka@corelogic.pl>
2021-08-05 08:36:36 +0200

    add RandomizeFood previews

--- app/src/main/java/pl/gungnir/fooddecider/ui/screens/randomizeFood/RandomizeFood.kt
+++ app/src/main/java/pl/gungnir/fooddecider/ui/screens/randomizeFood/RandomizeFood.kt
@@ -5,14 +5,18 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import org.koin.java.KoinJavaComponent.inject
 import pl.gungnir.fooddecider.R
 import pl.gungnir.fooddecider.ui.mics.FoodAnimationOnClick
+import pl.gungnir.fooddecider.ui.mics.nonRippleClickable
 import pl.gungnir.fooddecider.util.RANDOM_FOOD_TIME
 
 @ExperimentalAnimationApi
@@ -39,6 +43,13 @@ fun RandomizeFood() {
 
 }
 
+@ExperimentalAnimationApi
+@Preview(showBackground = true)
+@Composable
+private fun RandomizeFoodView() {
+    RandomizeFood()
+}
+
 @Composable
 fun RandomizeResult(result: Result) {
     val text = when (result) {
@@ -63,5 +74,23 @@ fun RandomizeResult(result: Result) {
             style = MaterialTheme.typography.h4,
             modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.space_xxLarge))
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun RandomizeResultView() {
+    val (result, setResult) = remember {
+        mutableStateOf<Result>(Result.Empty)
+    }
+    Box(modifier = Modifier.nonRippleClickable {
+        val newResult = when (result) {
+            Result.Empty -> Result.Loading
+            Result.Loading -> Result.Success("result")
+            else -> Result.Loading
+        }
+        setResult(newResult)
+    }) {
+        RandomizeResult(result = result)
     }
 }
