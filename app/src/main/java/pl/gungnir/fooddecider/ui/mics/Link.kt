fcc21f88cafc8bf5ec6cfd9dee9eda21b80cfa9d
Magdalena Rowicka <mrowicka@corelogic.pl>
2021-08-05 07:58:34 +0200

    add Link previews

--- app/src/main/java/pl/gungnir/fooddecider/ui/mics/Link.kt
+++ app/src/main/java/pl/gungnir/fooddecider/ui/mics/Link.kt
@@ -1,12 +1,16 @@
 package pl.gungnir.fooddecider.ui.mics
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import kotlin.random.Random
 
 @Composable
 fun Link(
@@ -24,5 +28,17 @@ fun Link(
         style = MaterialTheme.typography.h4,
         fontWeight = FontWeight.Bold,
         color = MaterialTheme.colors.primary
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LinkView() {
+    val (text, changeText) = remember { mutableStateOf("Link text. Click to random") }
+
+    Link(
+        modifier = Modifier.fillMaxWidth(),
+        text = text,
+        onClick = { changeText("Link text. Random number ${Random.nextInt(0, 10)}") }
     )
 }
